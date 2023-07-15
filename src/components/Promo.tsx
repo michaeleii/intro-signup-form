17c6b503b16d53e15763ade61e3de69b937efccc
Michael Lei <michaelleie0ca@gmail.com>
2023-07-14 17:04:00 -0700

    Fix styling

--- src/components/Promo.tsx
+++ src/components/Promo.tsx
@@ -1,8 +1,8 @@
 function Promo() {
   return (
     <div className="mb-5 rounded-lg bg-[hsl(248_32%_49%)] p-5 text-center text-white drop-shadow-lg">
-      <span className="font-bold">Try it free 7 days</span> then <br /> $20/mo.
-      thereafter
+      <span className="font-bold">Try it free 7 days</span> then{" "}
+      <span className="block xl:inline-block">$20/mo. thereafter</span>
     </div>
   );
 }
